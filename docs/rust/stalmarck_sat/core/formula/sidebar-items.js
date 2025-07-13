d58c8daf463a44cbf7c6c5b8ed8e6a959bba63bd
Liam Davis <davisliam123@gmail.com>
2025-07-12 20:15:01 -0400

    Added autodocs for Rust and Python

---
+++ docs/rust/stalmarck_sat/core/formula/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ImplicationFormula","TripletVar"],"struct":["Formula","TripletFormula"]};
---
+++ docs/rust/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["stalmarck_sat",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"stalmarck_sat/error/enum.Error.html\" title=\"enum stalmarck_sat::error::Error\">Error</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[285]}
